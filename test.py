a90498bf29a1b5e3e543226b6f7c8929baeac0f6
Dibyendu Biswas <67157274+dibyendubiswas1998@users.noreply.github.com>
2024-11-18 11:23:42 +0530

    Print the diamond pattern

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
 print("Hey Dibyendu !!!")
+
+# Print the diamond pattern
+def print_diamond_pattern(n):
+    # Upper half of the diamond
+    for i in range(n):
+        print(" " * (n - i - 1) + "*" * (2 * i + 1))
+    
+    # Lower half of the diamond
+    for i in range(n - 2, -1, -1):
+        print(" " * (n - i - 1) + "*" * (2 * i + 1))
+
+# Change 'n' to control the size of the diamond pattern
+n = 10
+print_diamond_pattern(n)
